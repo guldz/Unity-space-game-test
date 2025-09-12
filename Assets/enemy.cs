a3a537258caaf689627433f2799e2714716cdf31
unknown <vt25ador@axevalla.fhsk.se>
2025-09-12 11:57:45 +0200

    enemy added

---
+++ Assets/enemy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    
+        if(transform.position.y <= -5.5f) 
+        {
+            transform.position = new Vector3 (Random.Range (-8, 8), 5.5f, 0);
+        }
+    }
+    private void OnTriggerEnter2d(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Hit: " + other); 
+        }
+        if(other.tag == "Laser")
+        {
+            Destroy(gameObject); 
+        }
+        
+    }
+}
