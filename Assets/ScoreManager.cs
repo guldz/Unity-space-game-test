941deeb6b756f7b0b6e544a00aa13d71277e2c86
Lucas Klevvall <ht24kllu@axevalla.fhsk.se>
2025-10-03 11:55:42 +0200

    poäng system funkar inte

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ScoreManager : MonoBehaviour
+{
+    //public static ScoreManager instance;
+
+    public TMP_Text scoreText;
+    
+    int score;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateScore(1);
+    }
+    
+
+    // Update is called once per frame
+    public void UpdateScore(int points)
+    {
+        Debug.Log("TEST");
+        score += points;
+        scoreText.text = "Score: " + score; 
+    }
+}
--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     public float bulletSpeed = 1.0f; 
+    public GameObject scoremanager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
       Destroy(gameObject, 1); 
-        
+      scoremanager = GameObject.Find("Canvas");  
+      
     }
 
     // Update is called once per frame
@@ -18,6 +20,8 @@ public class bullet : MonoBehaviour
     }
 private void OnCollisionEnter2D(Collision2D collision) 
 {
+  scoremanager.GetComponent<ScoreManager>().UpdateScore(10);
   Destroy(gameObject); 
 }
+
 }
